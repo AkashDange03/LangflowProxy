ebfdb6d0623bf41fb6294fa80c875d0cb718198a
Akash_03 <akash.dange0307@gmail.com>
2025-01-07 20:27:55 +0530

    added start script commit

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ import 'dotenv/config'
 
 const app = express()
 
+
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
 
 
 app.get("/",(req,res)=>{
@@ -50,6 +53,6 @@ app.post("/chat",async (req,res)=>{
     }
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening to port ${process.env.PORT} `);
+app.listen(port,()=>{
+    console.log(`Listening to port ${port}`);
 })
